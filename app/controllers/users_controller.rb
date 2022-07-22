a10167034d94596849cd0d75305257999e1255fd
MariaJoseWelkner <josewelkner@gmail.com>
2022-07-22 13:44:47 -0400

    agregando heros

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def login
+  end
+
+  def logout
+  end
+
+  def register
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/login'
+  get 'users/logout'
+  get 'users/register'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get  "pages/test"
   get  "users/login"
   get  "pages/about_us"
   root "pages#index"
+
 end
